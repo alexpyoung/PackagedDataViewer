a0324ffc84e6a980735d60c111a5129164d50468
Alex Young <alexyoung1992@gmail.com>
2017-06-23 19:02:52 -0400

    feat(command): add more helpful descriptions for options

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,9 +6,9 @@ import TextTable
 
 command(
     Argument<String>("file", description: "Path to database file"),
-    Option<String>("type", "event", description: "Domain model type to query"),
-    Option<String>("s", "nil", description: "Timestamp sort order"),
-    Option<String>("b", "false", description: "Batch events by stream ID")
+    Option<String>("type", "event", description: "[event] - Domain model type to query"),
+    Option<String>("s", "nil", description: "[asc, desc] - Timestamp sort order"),
+    Option<String>("b", "false", description: "[true, false] - Batch events by stream ID")
 ) { filePath, modelType, sortOrderOrNil, isBatchingByStream in
 
     let events: [Event] = try Connection(filePath).fetchAll(for: modelType)
