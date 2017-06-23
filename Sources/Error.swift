086948bd99f33882b680a7af2f84e1d12068c535
Alex Young <alexyoung1992@gmail.com>
2017-06-23 15:18:25 -0400

    feat(PackagedDataViewerError): create basic JSON serialization error enumerations

---
+++ Sources/Error.swift
@@ -0,0 +1,12 @@
+//
+//  Error.swift
+//  PackagedDataViewer
+//
+//  Created by Alex Young on 6/23/17.
+//
+
+enum PackagedDataViewerError: Error {
+
+    case invalidJSONObject
+    case invalidJSONProperty
+}
