c9e02415a5004d5ba5ddeaa7667351869960764f
Alex Young <alexyoung1992@gmail.com>
2017-06-23 15:17:02 -0400

    feat(Event): create struct with minimum properties

---
+++ Sources/Event.swift
@@ -0,0 +1,12 @@
+//
+//  Event.swift
+//  PackagedDataViewer
+//
+//  Created by Alex Young on 6/23/17.
+//
+
+struct Event {
+    let id: String
+    let streamId: String
+    let timestamp: String
+}
