2c695526e6db15dc28292c044eadb807a52fe074
Alex Young <alexyoung1992@gmail.com>
2017-06-23 15:18:54 -0400

    feat(Event): implement Model protocol

--- Sources/Event.swift
+++ Sources/Event.swift
@@ -5,8 +5,27 @@
 //  Created by Alex Young on 6/23/17.
 //
 
-struct Event {
+import Foundation
+import SQLite
+
+struct Event: Model {
     let id: String
     let streamId: String
     let timestamp: String
+
+    init(blob: SQLite.Blob) throws {
+        let data = Data(bytes: blob.bytes, count: blob.bytes.count)
+        guard let json = try JSONSerialization.jsonObject(with: data) as? [AnyHashable:AnyHashable] else {
+            throw PackagedDataViewerError.invalidJSONObject
+        }
+        guard let id = json["_id"] as? String,
+            let streamId = json["stream_id"] as? String,
+            let timestamp = json["timestamp"] as? String
+        else {
+            throw PackagedDataViewerError.invalidJSONProperty
+        }
+        self.id = id
+        self.streamId = streamId
+        self.timestamp = timestamp
+    }
 }
