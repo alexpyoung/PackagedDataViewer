f60457a12f7eb91c38dc5f4df678f8e99f44ac8c
Alex Young <alexyoung1992@gmail.com>
2017-06-23 15:22:49 -0400

    feat(main): implement event list query

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,3 +2,11 @@ import Commander
 import Foundation
 import SQLite
 import TextTable
+
+let cache = Table("cache")
+let data = Expression<SQLite.Blob>("data")
+let type = Expression<String>("_type")
+
+let query = cache
+    .select(data)
+    .filter(type == "event")
