3b24c3f400a579cad6585b2e62f03c6c4844fa20
Alex Young <alexyoung1992@gmail.com>
2017-06-23 11:25:42 -0400

    feat(Package): add Commander, SQLite, TextTable dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "PackagedDataViewer"
+    name: "PackagedDataViewer",
+    dependencies: [
+        .Package(url: "https://github.com/kylef/Commander.git", Version(0,6,1)),
+        .Package(url: "https://github.com/stephencelis/SQLite.swift.git", Version(0, 11, 3)),
+        .Package(url: "https://github.com/alexpyoung/TextTable.git", Version(1, 0, 0, prereleaseIdentifiers: ["alpha", "5"]))
+    ]
 )
