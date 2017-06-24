71537ab13a8bfb072ac608b1119f1c3f08d113af
Alex Young <alexyoung1992@gmail.com>
2017-06-24 19:08:51 -0400

    fix(Stream): make id and gameId optional

--- Sources/Model.swift
+++ Sources/Model.swift
@@ -9,7 +9,5 @@ import SQLite
 
 protocol Model {
 
-    var id: String { get }
-
     init(blob: SQLite.Blob) throws
 }
--- Sources/Stream.swift
+++ Sources/Stream.swift
@@ -10,20 +10,15 @@ import SQLite
 
 struct Stream: Model {
 
-    let id: String
-    let gameId: String
+    let id: String?
+    let gameId: String?
 
     init(blob: SQLite.Blob) throws {
         let data = Data(bytes: blob.bytes, count: blob.bytes.count)
         guard let json = try JSONSerialization.jsonObject(with: data) as? [AnyHashable:AnyHashable] else {
             throw PackagedDataViewerError.invalidJSONObject
         }
-        guard let id = json["_id"] as? String,
-            let gameId = json["game_id"] as? String
-        else {
-            throw PackagedDataViewerError.invalidJSONProperty
-        }
-        self.id = id
-        self.gameId = gameId
+        self.id = json["_id"] as? String
+        self.gameId = json["game_id"] as? String
     }
 }
