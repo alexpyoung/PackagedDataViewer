4efbe6ed3bd94f9aa80afb6dcd742f2f7dc35544
Alex Young <alexyoung1992@gmail.com>
2017-06-24 13:33:30 -0400

    feat(main): implement filtering by stream ID from CLI

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,13 +4,22 @@ import SQLite
 import TextTable
 
 
+// FIXME: Type safety!!!
 command(
-    Argument<String>("file", description: "Path to database file"),
     Option<String>("type", "event", description: "[event] - Domain model type to query"),
     Option<String>("s", "nil", description: "[asc, desc] - Timestamp sort order"),
-    Option<String>("b", "false", description: "[true, false] - Batch events by stream ID")
-) { filePath, modelType, sortOrderOrNil, isBatchingByStream in
-
-    let events: [Event] = try Connection(filePath).fetchAll(for: modelType)
-    print(events, sorted: SortOrder(rawValue: sortOrderOrNil), shouldBatch: isBatchingByStream == "true")
+    Option<String>("b", "false", description: "[true, false] - Batch events by stream ID"),
+    Option<String>("sid", "nil", description: "Stream ID"),
+    Argument<String>("file", description: "Path to database file")
+) { modelType, sortOrderOrNil, isBatchingByStream, streamIdOrNil, filePath in
+    let events: [Event]
+    // FIXME: Type safety!!!
+    if streamIdOrNil != "nil" {
+        let streamId = streamIdOrNil
+        events = try Connection(filePath).fetchEvents(withStreamId: streamId)
+        print(events, sorted: .ascending, shouldBatch: true)
+    } else {
+        events = try Connection(filePath).fetchAll(for: modelType)
+        print(events, sorted: SortOrder(rawValue: sortOrderOrNil), shouldBatch: isBatchingByStream == "true")
+    }
 }.run()
