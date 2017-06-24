d857a5f358ff4e2e660906ebff27eeee5e59ed71
Alex Young <alexyoung1992@gmail.com>
2017-06-24 18:45:11 -0400

    refactor(main): use group to organize commands

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,25 +4,6 @@ import SQLite
 import TextTable
 
 
-// FIXME: Type safety!!!
-command(
-    Option<String>("type", "event", description: "[event] - Domain model type to query"),
-    Option<String>("s", "nil", description: "[asc, desc] - Timestamp sort order"),
-    Flag("b", description: "[true, false] - Batch events by stream ID", default: false),
-    Option<String>("sid", "nil", description: "Stream ID"),
-    VariadicArgument<String>("files", description: "Variadic list of paths to database files")
-) { modelType, sortOrderOrNil, isBatchingByStream, streamIdOrNil, filePaths in
-    try filePaths.forEach{
-        print("Reading \($0)")
-        let events: [Event]
-        // FIXME: Type safety!!!
-        if streamIdOrNil != "nil" {
-            let streamId = streamIdOrNil
-            events = try Connection($0).fetchEvents(withStreamId: streamId)
-            print(events, sorted: .ascending, shouldBatch: true)
-        } else {
-            events = try Connection($0).fetchAll(for: modelType)
-            print(events, sorted: SortOrder(rawValue: sortOrderOrNil), shouldBatch: isBatchingByStream)
-        }
-    }
-}.run()
+let group = Group()
+group.addCommand("events", eventsCommand)
+group.run()
