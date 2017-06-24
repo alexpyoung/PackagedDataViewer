71e055d485821dab4ea41ff5a285563a074f0ca8
Alex Young <alexyoung1992@gmail.com>
2017-06-24 19:15:15 -0400

    feat(Stream): add homeId and awayId properties

--- Sources/Stream.swift
+++ Sources/Stream.swift
@@ -12,13 +12,22 @@ struct Stream: Model {
 
     let id: String?
     let gameId: String?
+    let homeId: String
+    let awayId: String
 
     init(blob: SQLite.Blob) throws {
         let data = Data(bytes: blob.bytes, count: blob.bytes.count)
         guard let json = try JSONSerialization.jsonObject(with: data) as? [AnyHashable:AnyHashable] else {
             throw PackagedDataViewerError.invalidJSONObject
         }
+        guard let homeId = json["home_id"] as? String,
+            let awayId = json["away_id"] as? String
+        else {
+            throw PackagedDataViewerError.invalidJSONProperty
+        }
         self.id = json["_id"] as? String
         self.gameId = json["game_id"] as? String
+        self.homeId = homeId
+        self.awayId = awayId
     }
 }
