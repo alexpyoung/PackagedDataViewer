ee4c69445a6503db16d8be683ed934b9003628d6
Alex Young <alexyoung1992@gmail.com>
2017-09-19 21:30:12 -0400

    feat(Package.swift): use upToNextMinor where possible

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "PackagedDataViewer",
     dependencies: [
-        .package(url: "https://github.com/kylef/Commander.git", .exact("0.6.0")),
-        // FIXME: Use .upToMinor(from: "0.11.4") once released
+        .package(url: "https://github.com/kylef/Commander.git", .upToNextMinor(from: "0.6.0")),
+        // FIXME: Use .upToNextMinor(from: "0.11.4") once released
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", .branch("release")),
         .package(url: "https://github.com/alexpyoung/TextTable.git", from: "1.0.0-alpha.5")
     ],
