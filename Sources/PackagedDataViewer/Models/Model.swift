76460954026581b0c369a50152da7737ee4e117c
Alex Young <alexyoung1992@gmail.com>
2017-09-19 21:35:55 -0400

    feat(Model): implement dateFormatter property

--- Sources/PackagedDataViewer/Models/Model.swift
+++ Sources/PackagedDataViewer/Models/Model.swift
@@ -5,11 +5,23 @@
 //  Created by Alex Young on 6/23/17.
 //
 
+import Foundation
 import SQLite
 
 protocol Model {
 
     static var typeIdentifier: String { get }
+    static var dateFormatter: DateFormatter { get }
 
     init(blob: SQLite.Blob) throws
+}
+
+extension Model {
+
+    static var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        return formatter
+    }
 }
