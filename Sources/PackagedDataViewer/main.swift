f533a2d09893d86578e7e3130b775f52e57528c0
Alex Young <alexyoung1992@gmail.com>
2018-03-16 00:24:41 -0400

    feat(main): add games command

--- Sources/PackagedDataViewer/main.swift
+++ Sources/PackagedDataViewer/main.swift
@@ -5,6 +5,7 @@ import TextTable
 
 let group = Group()
 group.addCommand("events", eventsCommand)
+group.addCommand("games", gamesCommand)
 group.addCommand("streams", streamsCommand)
 group.addCommand("teams", teamsCommand)
 group.run()
