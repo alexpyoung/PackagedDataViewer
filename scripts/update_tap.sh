b6106b26031d9d4440fe9a9fdc402b5c03a590c5
Alex Young <alexyoung1992@gmail.com>
2018-03-18 11:24:23 -0400

    refactor(update_tap): accept generic params

--- scripts/update_tap.sh
+++ scripts/update_tap.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+if [ $# -ge 3 ]
+  then
+    echo 'Cannot continue without a tag, tarball filename, & checksum'
+    exit 0
+fi
+
 git clone git://github.com/alexpyoung/homebrew-tools.git
 cd homebrew-tools/Formula
 
+TAG=$1
+TARBALL_FILENAME=$2
+CHECKSUM=$3
 FILENAME="packageddataviewer.rb"
 REPO_URL="https://github.com/alexpyoung/PackagedDataViewer"
 
 echo "class Packageddataviewer < Formula" > $FILENAME
 echo "  desc \"A CLI for SQLite querying\"" >> $FILENAME
 echo "  homepage \"$REPO_URL\"" >> $FILENAME
-echo "  url \"$REPO_URL/releases/download/$TRAVIS_TAG/$TARBALL_FILENAME\"" >> $FILENAME
+echo "  url \"$REPO_URL/releases/download/$TAG/$TARBALL_FILENAME\"" >> $FILENAME
 echo "  sha256 \"$CHECKSUM\"" >> $FILENAME
-echo "  version \"$TRAVIS_TAG\"" >> $FILENAME
+echo "  version \"$TAG\"" >> $FILENAME
 echo "  bottle :unneeded" >> $FILENAME
 echo "  def install" >> $FILENAME
 echo "    bin.install \"packageddataviewer\"" >> $FILENAME
@@ -21,5 +30,5 @@ echo "    system \"#{bin}/packageddataviewer\", \"--version\"" >> $FILENAME
 echo "  end" >> $FILENAME
 echo "end" >> $FILENAME
 
-git commit -am "feat(packageddataviewer): $TRAVIS_TAG"
+git commit -am "feat(packageddataviewer): $TAG"
 git push origin master
