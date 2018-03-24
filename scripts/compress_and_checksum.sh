0faf0b5b0d98b570b4c86ae1ce48ef43879648eb
Alex Young <alexyoung1992@gmail.com>
2018-03-24 13:58:42 -0400

    refactor(compress_and_checksum): use local vars and checksum

--- scripts/compress_and_checksum.sh
+++ scripts/compress_and_checksum.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 1 ]
-  then
-    echo 'Cannot continue without a tag'
-    exit 2
-fi
+main() {
+    if [ $# -lt 1 ]; then
+        echo 'Cannot continue without a tag'
+        exit 2
+    fi
+    local -r TAG=$1
 
-TAG=$1
+    # Run from project root
+    cd .build/release
+    export TARBALL_FILENAME="PackagedDataViewer-$TAG.tar.gz"
+    export CHECKSUMS_FILENAME="PackagedDataViewer-$TAG-checksums.txt"
+    tar -czf $TARBALL_FILENAME PackagedDataViewer
+    export CHECKSUM=$(shasum -a 256 $TARBALL_FILENAME | awk '{printf $1}')
+    echo $CHECKSUM > $CHECKSUMS_FILENAME
+    echo $TARBALL_FILENAME >> $CHECKSUMS_FILENAME
+}
 
-# Run from project root
-cd .build/release
-export TARBALL_FILENAME="PackagedDataViewer-$TAG.tar.gz"
-export CHECKSUMS_FILENAME="PackagedDataViewer-$TAG-checksums.txt"
-tar -czf $TARBALL_FILENAME PackagedDataViewer
-export CHECKSUM=$(shasum -a 256 $TARBALL_FILENAME | awk '{printf $1}')
-echo $CHECKSUM > $CHECKSUMS_FILENAME
-echo $TARBALL_FILENAME >> $CHECKSUMS_FILENAME
+main $1
